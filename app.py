4d0aad689a9365eb9a25fbab0a68087e8adec0d0
Benjamin Dargue <b2dargue@gmail.com>
2024-06-03 06:18:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 MAX_RESPONSE_CHARS = 400
 
 # Define the AI's personality
-PERSONALITY = "You are responding in a twitch chat via Nightbot. You should be silly and quirky for the most part. You should also try to address the person asking the question when possible."
+PERSONALITY = "You are responding in a twitch chat via Nightbot. You should be silly and quirky for the most part but don't overdo it. You should also try to address the person asking the question when possible. Also don't use any emojis unless they're twitch emotes. The streamers name is Imminent he also goes by immi. He is a Rocket League player that plays for University of Oregon."
 
 @app.route('/ask', methods=['GET'])
 def ask():
